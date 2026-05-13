be03d3c432281c4eadac1197e8043706120a27d1
Lera245 <bum073881@gmail.com>
2026-05-13 16:41:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,6 @@ public class Main {
 
     }
 }
-
 interface Tovar {
     void t(String name, String price, String reiting);
 }
@@ -18,83 +17,70 @@ interface Tovar {
 interface User {
     void t1(String login, String password);
 }
-
 class Myaso implements Tovar {
     String name;
     String price;
     String reiting;
 
-    Pelmeni(String name, String price, String reiting) {
+    Myaso(String name, String price, String reiting) {
         this.name = name;
         this.price = price;
         this.reiting = reiting;
     }
-
+    @Override
     public void t(String name, String price, String reiting) {
         System.out.println("название -" + name + ", цена -" + price + ", рейтинг -" + reiting);
     }
 }
-
 class Pelmeni extends Myaso {
     Pelmeni(String name, String price, String reiting) {
         super(name, price, reiting);
     }
-
     @Override
     public void t(String name, String price, String reiting) {
         System.out.println("название -" + name + ", цена -" + price + ", рейтинг -" + reiting);
     }
 }
-
 class Kotleta extends Myaso {
     Kotleta(String name, String price, String reiting) {
         super(name, price, reiting);
     }
-
     @Override
     public void t(String name, String price, String reiting) {
         System.out.println("название -" + name + ", цена -" + price + ", рейтинг -" + reiting);
     }
 }
-
 class People implements User {
     String fio;
     int age;
     String post;
-
     People(String fio, int age, String post) {
         this.fio = fio;
         this.age = age;
         this.post = post;
     }
-
     public void t1(String login, String password) {
         System.out.println("login" + login + ", password " + password);
     }
 }
-
 class Teacher extends People {
     Teacher(String fio, int age, String post) {
         super(fio, age, post);
     }
-
     @Override
     public void t1(String login, String password) {
         System.out.println("login" + login + ", password " + password);
     }
 }
-
 class Student extends People {
     Student(String fio, int age, String post) {
         super(fio, age, post);
     }
-
     @Override
     public void t1(String login, String password) {
         System.out.println("login" + login + ", password " + password);
     }
 }
-
 class Pocupatel implements Tovar, User {
     String name;
     String price;
@@ -102,7 +88,6 @@ class Pocupatel implements Tovar, User {
     String fio;
     int age;
     String post;
-
     Pocupatel(String name, String price, String reiting, String fio, int age, String post) {
         this.name = name;
         this.price = price;
@@ -111,15 +96,12 @@ class Pocupatel implements Tovar, User {
         this.age = age;
         this.post = post;
     }
-
     public void t(String name, String price, String reiting) {
         System.out.println("название -" + name + ", цена -" + price + ", рейтинг -" + reiting);
     }
-
     public void t1(String login, String password) {
         System.out.println("login" + login + ", password " + password);
     }
-
     public void e(String name, String fio) {
         System.out.printf("покупатель %s купил товар %s", fio, name);
     }
